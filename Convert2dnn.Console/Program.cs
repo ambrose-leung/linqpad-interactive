43feed6fe950995e7aa63155384ba9c0842bccd7
Ambrose Leung <no@email.provided>
2022-09-22 11:09:40 -0700

    print status message, rename namespace

--- Convert2dnn.Console/Program.cs
+++ Convert2dnn.Console/Program.cs
@@ -1,19 +1,20 @@
 ﻿using LINQPad_Interactive.Convert.Lib;
 using System;
 
-namespace Convert2dnn.Console
+namespace Convert2dnn_Console
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             if (args.Length > 0) {
-                FileConvert.ConvertToInteractiveNotebook(args[0]);
+                var status = FileConvert.ConvertToInteractiveNotebook(args[0]);
+                Console.WriteLine(status);
             }
 
             else
             {
-                System.Console.WriteLine("usage: ..exe \"inputPath.linq\"");
+                Console.WriteLine("usage: ..exe \"inputPath.linq\"");
             }
         }
     }
