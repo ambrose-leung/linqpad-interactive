c4c1114a6b36688231c11986350ba409a2a626c6
Ambrose Leung <no@email.provided>
2022-09-22 09:32:02 -0700

    adding console app for use with VS Code extension

---
+++ Convert2dnn.Console/Program.cs
@@ -0,0 +1,20 @@
+﻿using LINQPad_Interactive.Convert.Lib;
+using System;
+
+namespace Convert2dnn.Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 0) {
+                FileConvert.ConvertToInteractiveNotebook(args[0]);
+            }
+
+            else
+            {
+                System.Console.WriteLine("usage: ..exe \"inputPath.linq\"");
+            }
+        }
+    }
+}
